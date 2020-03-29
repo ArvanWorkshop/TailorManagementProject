c8c4c85088fde14004b6d261b8f63ee7b6f6142d
ArvanWorkshop <arvanbishwas504@gmail.com>
2020-03-29 13:08:37 +0600

    update

--- accounts/views.py
+++ accounts/views.py
@@ -115,7 +115,6 @@ def accountSettings(request):
 
 
 @login_required(login_url='login')
-@allowed_users(allowed_roles=['admin'])
 def products(request):
 	products = Product.objects.all()
 	return render(request, 'accounts/products.html', {'products':products})
